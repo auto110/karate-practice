80aa5037f4f124ca7a242e546ebb9622a50a2ab3
qiaolin chen <itqiaolin@gmail.com>
2020-09-04 14:58:34 +0800

    修改用例执行环境为test

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -3,13 +3,14 @@ function fn() {
   karate.log('karate.env system property was:', env);
   if (!env) {
     //如果env未设置，默认使用localhost环境
-    env = 'dev';
+    env = 'test';
   }
 
   //默认环境配置
   var config = {
     env: env,
-    vMockURL: 'http://localhost:9191'
+    vMockURL: 'http://localhost:9191',
+    reqresURL:'https://reqres.in/api'
   }
 
   if(env == 'localhost')
@@ -17,7 +18,7 @@ function fn() {
     config.vMockURL = 'http://localhost:9191'
     config.dbConfig = karate.read('classpath:DBConfig_test.json')
   }
-  if (env == 'dev') {
+  if (env == 'test') {
     // customize
     // e.g. config.foo = 'bar';
     config.vMockURL = 'http://172.16.0.115:9191'
@@ -31,6 +32,8 @@ function fn() {
   var loginSession = karate.callSingle("classpath:vmock/APIs/login.feature@login_success", config)
   config.JSESSIONID = loginSession.JSESSIONID
   config.rememberMe = loginSession.rememberMe
+
+  config.reqresURL = 'https://reqres.in/api'
 
   //设置请求超时时间
   karate.configure('connectTimeout', 10000);
