dadf5d00e2d7255ebf0c38bd9fe9d0eea0785f01
pengzhengfa <pengzhengfa@biosan.cn>
2020-09-09 15:07:55 +0800

    http测试工具类优化

--- src/test/java/utils/HttpUtil.java
+++ src/test/java/utils/HttpUtil.java
@@ -5,12 +5,14 @@ import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.*;
+
 /**
  * @author pengzhengfa
  */
@@ -25,16 +27,16 @@ public class HttpUtil {
     /**
      * getCases接口测试
      */
-    public static void getCaseData() {
-        
-        String loginURL = "http://xcx.sys.saas.biosan.cn/treat/applet/getCases";
-        //创建一个httppost请求
-        httppost = new HttpPost(loginURL);
+    public static void requestPost(String requestUrl, Map<String, String> param) {
+        httppost = new HttpPost(requestUrl);
         //创建Post请求参数
         List<NameValuePair> list = new ArrayList<NameValuePair>();
-        list.add(new BasicNameValuePair("caseIds", "['b5e137c7-dbbf-4d36-a9ab-3854878f3d83']"));
-        list.add(new BasicNameValuePair("originType", "miniprogram"));
-        list.add(new BasicNameValuePair("usertoken", "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJiaW9zYW4iLCJzdWIiOiJ0b2tlbiIsImF1ZCI6ImJpb3Nhbi1zYWFzIiwianRpIjoiODA0NzY1ZGYzMWY3NGFlZjhkOWIxZGZjMGM5YjM5MDcifQ.C2NINu6Thl8HtqAlSSIP1RBfg6AgiY7aQ1bygV3iaVQYTryBd83AiLTmUM2aXCGEdSKf8Kuw_HACgUmFl27Ww3IfFE6IIqdQNi-QMkflPvKlQUFBIOsipuvuEnMRolzHtVU32tNo2dPgeE1S91c0HaLAcAUlUXWdJrVjTpxXHjxTJ-NoI9jh507r7cBHd0cg78T4lCv-vQvLxHS6uaexBacFE6oeHLW7ADljiG8S6nmkRHoRYK30sZQbJQLABex8Ky5uDGk6tXqrXvnY8_B75M4IwlJLvxQjT70GVwbx5UcaKlfJZ7GseOAxPZnNiti7x35rIwmVAM_myDxVcvaiNw"));
+            for(Map.Entry<String, String> entry : param.entrySet()){
+                String requestKey = entry.getKey();
+                String requestValue = entry.getValue();
+                BasicNameValuePair basicNameValuePair = new BasicNameValuePair(requestKey, requestValue);
+                list.add(basicNameValuePair);
+        }
         try {
             httppost.setEntity(new UrlEncodedFormEntity(list, "UTF-8"));
             response = httpClient.execute(httppost);
@@ -45,5 +47,12 @@ public class HttpUtil {
             e.printStackTrace();
         }
         httppost.releaseConnection();
+    }
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("caseIds", "['b5e137c7-dbbf-4d36-a9ab-3854878f3d83']");
+        map.put("originType", "miniprogram");
+        map.put("usertoken", "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJiaW9zYW4iLCJzdWIiOiJ0b2tlbiIsImF1ZCI6ImJpb3Nhbi1zYWFzIiwianRpIjoiODA0NzY1ZGYzMWY3NGFlZjhkOWIxZGZjMGM5YjM5MDcifQ.C2NINu6Thl8HtqAlSSIP1RBfg6AgiY7aQ1bygV3iaVQYTryBd83AiLTmUM2aXCGEdSKf8Kuw_HACgUmFl27Ww3IfFE6IIqdQNi-QMkflPvKlQUFBIOsipuvuEnMRolzHtVU32tNo2dPgeE1S91c0HaLAcAUlUXWdJrVjTpxXHjxTJ-NoI9jh507r7cBHd0cg78T4lCv-vQvLxHS6uaexBacFE6oeHLW7ADljiG8S6nmkRHoRYK30sZQbJQLABex8Ky5uDGk6tXqrXvnY8_B75M4IwlJLvxQjT70GVwbx5UcaKlfJZ7GseOAxPZnNiti7x35rIwmVAM_myDxVcvaiNw");
+        HttpUtil.requestPost("http://xcx.sys.saas.biosan.cn/treat/applet/getCases",map);
     }
 }
